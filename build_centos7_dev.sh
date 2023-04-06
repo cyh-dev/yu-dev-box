d544b77d558555600bc960d96c2d6c57e1eebbf2
yuhao.chen <yuhao.chen@shopee.com>
2023-04-06 18:56:24 +0800

    add tools script

--- build_centos7_dev.sh
+++ build_centos7_dev.sh
@@ -14,7 +14,8 @@ chsh -s /bin/zsh
 
 # python2
 pip install $PIP_INDEX -U pip==20.3.4 setuptools==44.1.1 wheel==0.36.2
-pip install --force-reinstall certifi && sed -i '/DST Root CA X3/,+27d' /usr/lib/python2.7/site-packages/certifi/cacert.pem
+pip install --force-reinstall certifi
+sed -i '/DST Root CA X3/,+27d' /usr/lib/python2.7/site-packages/certifi/cacert.pem
 
 # python3
 $( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%N}}" )" >/dev/null 2>&1 && pwd )/python/build_python3-centos7.sh
