a96819daae525144ae70100b91c0d11e67f6d685
yuhao.chen <yuhao.chen@shopee.com>
2023-04-15 21:40:16 +0800

    add centos7 script

--- centos/centos7_upgrade_kennel.sh
+++ centos/centos7_upgrade_kennel.sh
@@ -3,7 +3,7 @@ yum -y update
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
-yum --enablerepo=elrepo-kernel install kernel-ml
+yum --enablerepo=elrepo-kernel install -y kernel-ml
 
 grub2-set-default 0
 
